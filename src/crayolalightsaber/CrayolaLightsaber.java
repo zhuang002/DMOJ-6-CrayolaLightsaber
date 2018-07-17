7062eae22cf4da2199743caa5b87c420eb812ab7
zhuang002 <zhuang001@gmail.com>
2018-07-16 23:07:21 -0400

    performance tuned

--- src/crayolalightsaber/CrayolaLightsaber.java
+++ src/crayolalightsaber/CrayolaLightsaber.java
@@ -5,8 +5,10 @@
  */
 package crayolalightsaber;
 
-
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.Scanner;
+import java.util.Vector;
 
 /**
  *
@@ -14,60 +16,144 @@ import java.util.Scanner;
  */
 public class CrayolaLightsaber {
 
+    static HashMap<MyKey, Integer> recursiveBackup = new HashMap();
+
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        Scanner sc=new Scanner(System.in);
-        int n=sc.nextInt();
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
         sc.nextLine();
-        int[] markers=new int[6];
-        String[] sAl=sc.nextLine().split(" ");
-        for (int i=0;i<6;i++) markers[i]=0;
-        for (int i=0;i<n;i++) {
-            String color=sAl[i];
+        int[] markers = new int[6];
+        String[] sAl = sc.nextLine().split(" ");
+        for (int i = 0; i < 6; i++) {
+            markers[i] = 0;
+        }
+        for (int i = 0; i < n; i++) {
+            String color = sAl[i];
             switch (color) {
                 case "yellow":
-                    markers[0]+=1;
+                    markers[0] += 1;
                     break;
                 case "blue":
-                    markers[1]+=1;
+                    markers[1] += 1;
                     break;
                 case "red":
-                    markers[2]+=1;
+                    markers[2] += 1;
                     break;
                 case "black":
-                    markers[3]+=1;
+                    markers[3] += 1;
                     break;
                 case "green":
-                    markers[4]+=1;
+                    markers[4] += 1;
                     break;
                 case "orange":
-                    markers[5]+=1;
+                    markers[5] += 1;
                     break;
                 default:
                     break;
-                            
             }
-                    
+
         }
         
-        System.out.println(getStickLength(7,markers));
+        System.out.println(getStickLength(7, markers));
     }
 
     private static int getStickLength(int previousColor, int[] markers) {
-        int maxLength=0;
-        
-        for (int color=0;color<6;color++) {
-            if (color==previousColor) continue;
-            int count=markers[color];
-            if (count==0) continue;
-            markers[color]=count-1;
-            int length=getStickLength(color,markers)+1;
-            if (maxLength<length) maxLength=length;
-            markers[color]=count;
+        int[] changedColor=preprocessMarkers(markers);
+        if (changedColor!=null) {
+            int length=getStickLength(previousColor,markers);
+            markers[changedColor[0]]+=changedColor[1];
+            return length;
         }
+        MyKey key = new MyKey(previousColor, markers);
+        if (recursiveBackup.containsKey(key)) {
+            return recursiveBackup.get(key);
+        }
+
+        int maxLength = 0;
+
+        for (int color = 0; color < 6; color++) {
+            if (color == previousColor) {
+                continue;
+            }
+            int count = markers[color];
+            if (count == 0) {
+                continue;
+            }
+            markers[color] = count - 1;
+            
+            int length = getStickLength(color, markers) + 1;
+            if (maxLength < length) {
+                maxLength = length;
+            }
+            markers[color] = count;
+        }
+
+        recursiveBackup.put(key, maxLength);
         return maxLength;
     }
+
+    private static int[] preprocessMarkers(int[] markers) {
+        int[] changedColor=null;
+        int maxColor=-1;
+        int maxCount=-1;
+        int totalMarkers=0;
+        
+        for (int i=0;i<markers.length;i++) {
+            if (maxCount<markers[i]) {
+                maxCount=markers[i];
+                maxColor=i;
+            }
+            totalMarkers+=markers[i];
+        }
+        if (maxColor==-1) return null;
+        int otherMarkers=totalMarkers-maxCount;
+        if (maxCount>otherMarkers+1){
+            markers[maxColor]=otherMarkers+1;
+            changedColor=new int[2];
+            changedColor[0]=maxColor;
+            changedColor[1]=maxCount-otherMarkers-1;
+        }
+        return changedColor;
+    }
+
 }
+
+class MyKey {
+
+    int[] ar;
+
+    public MyKey(int color, int[] markers) {
+        ar = new int[markers.length + 1];
+        ar[0] = color;
+        for (int i = 0; i < markers.length; i++) {
+            ar[i + 1] = markers[i];
+        }
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        MyKey key = (MyKey) obj;
+        return (this.ar[0] == key.ar[0]
+                && this.ar[1] == key.ar[1]
+                && this.ar[2] == key.ar[2]
+                && this.ar[3] == key.ar[3]
+                && this.ar[4] == key.ar[4]
+                && this.ar[5] == key.ar[5]
+                && this.ar[6] == key.ar[6]);
+    }
+
+    @Override
+    public int hashCode() {
+        int hashCode = 1;
+        hashCode = 31 * hashCode + java.util.Arrays.hashCode(ar);
+        return hashCode;
+    }
+}
+/* 
+30
+red red red red red red red red red red red red red red red red red red red red orange yellow green blue black orange yellow green blue black
+*/
